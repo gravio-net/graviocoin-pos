6e54fbdb1414efbc7058bc4d126fa76af63101d6
szhelonkin <zhelonkin@gravio.net>
2020-12-07 16:11:00 +0300

    fix address

--- src/qt/receivecoinsdialog.cpp
+++ src/qt/receivecoinsdialog.cpp
@@ -99,7 +99,7 @@ void ReceiveCoinsDialog::setModel(WalletModel *_model)
         }
 
         // Set the button to be enabled or disabled based on whether the wallet can give out new addresses.
-        ui->receiveButton->setEnabled(model->canGetAddresses());
+        // ui->receiveButton->setEnabled(model->canGetAddresses());
 
         // Enable/disable the receive button if the wallet is now able/unable to give out new addresses.
         connect(model, &WalletModel::canGetAddressesChanged, [this] {
